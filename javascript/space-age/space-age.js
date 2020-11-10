e4660e1904aeb82f715ef64af6cda0d77feee442
André Brandão <andrenbrandao@gmail.com>
2020-11-10 07:33:32 -0300

    javascript/space-age: add solution

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,20 @@
+const EARTH_YEAR_IN_SECONDS = 31557600;
+
+const PLANET_TO_EARTH_YEARS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+const secondsToYears = seconds => seconds / EARTH_YEAR_IN_SECONDS;
+
+export const age = (planet, seconds) => {
+  const years = secondsToYears(seconds) / PLANET_TO_EARTH_YEARS[planet];
+
+  return Number(years.toFixed(2));
+};
