57f3d5330b83c412ed724ef5ba10db7c3fa22975
André Brandão <andrenbrandao@gmail.com>
2022-04-26 17:38:50 -0300

    go/gross-store: add solution

---
+++ go/gross-store/gross_store.go
@@ -0,0 +1,56 @@
+package gross
+
+// Units stores the Gross Store unit measurements.
+func Units() map[string]int {
+	units := map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+
+	return units
+}
+
+// NewBill creates a new bill.
+func NewBill() map[string]int {
+	emptyBill := map[string]int{}
+	return emptyBill
+}
+
+// AddItem adds an item to customer bill.
+func AddItem(bill, units map[string]int, item, unit string) bool {
+	value, exists := units[unit]
+	if !exists {
+		return false
+	}
+
+	bill[item] += value
+	return true
+}
+
+// RemoveItem removes an item from customer bill.
+func RemoveItem(bill, units map[string]int, item, unit string) bool {
+	unitValue, unitExists := units[unit]
+	itemValue, itemExists := bill[item]
+	newQuantity := itemValue - unitValue
+
+	if !unitExists || !itemExists || newQuantity < 0 {
+		return false
+	}
+
+	bill[item] = newQuantity
+
+	if newQuantity == 0 {
+		delete(bill, item)
+	}
+	return true
+}
+
+// GetItem returns the quantity of an item that the customer has in his/her bill.
+func GetItem(bill map[string]int, item string) (int, bool) {
+	value, exists := bill[item]
+	return value, exists
+}
