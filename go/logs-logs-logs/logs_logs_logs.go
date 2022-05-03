79a51b64a2686f34616f717d51a4909b5caf4473
André Brandão <andrenbrandao@gmail.com>
2022-05-02 22:52:19 -0300

    go/logs-logs-logs: add solution

---
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -0,0 +1,37 @@
+package logs
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+// Application identifies the application emitting the given log.
+func Application(log string) string {
+	applications := map[rune]string{
+		'❗': "recommendation",
+		'🔍': "search",
+		'☀': "weather",
+	}
+
+	for _, value := range log {
+		application, ok := applications[value]
+
+		if ok {
+			return application
+		}
+	}
+
+	return "default"
+}
+
+// Replace replaces all occurrences of old with new, returning the modified log
+// to the caller.
+func Replace(log string, oldRune, newRune rune) string {
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
+}
+
+// WithinLimit determines whether or not the number of characters in log is
+// within the limit.
+func WithinLimit(log string, limit int) bool {
+	return utf8.RuneCountInString(log) <= limit
+}
