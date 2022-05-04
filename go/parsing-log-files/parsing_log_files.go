f310477a3863e3aba2ee81bd5605670f016ff180
André Brandão <andrenbrandao@gmail.com>
2022-05-03 22:53:36 -0300

    go/parsing-log-files: add solution

---
+++ go/parsing-log-files/parsing_log_files.go
@@ -0,0 +1,47 @@
+package parsinglogfiles
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func IsValidLine(text string) bool {
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.MatchString(text)
+}
+
+func SplitLogLine(text string) []string {
+	re := regexp.MustCompile(`<[~*=-]*>`)
+	return re.Split(text, -1)
+}
+
+func CountQuotedPasswords(lines []string) int {
+	re := regexp.MustCompile(`(?i)".*password.*"`)
+
+	count := 0
+	for _, line := range lines {
+		if re.MatchString(line) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func RemoveEndOfLineText(text string) string {
+	re := regexp.MustCompile(`end-of-line\d+`)
+	return re.ReplaceAllString(text, "")
+}
+
+func TagWithUserName(lines []string) []string {
+	re := regexp.MustCompile(`User +(\w+\b)`)
+
+	for i, line := range lines {
+		match := re.FindStringSubmatch(line)
+		if match != nil {
+			lines[i] = fmt.Sprintf("[USR] %s %s", match[1], line)
+		}
+	}
+
+	return lines
+}
