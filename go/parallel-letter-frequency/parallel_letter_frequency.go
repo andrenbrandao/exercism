92017345d63b3fcfa34dd1aa9e72052edc032cae
André Brandão <andrenbrandao@gmail.com>
2022-06-17 23:33:22 -0300

    go/parallel-letter-frequency: add solution with channels

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -1,9 +1,5 @@
 package letter
 
-import (
-	"sync"
-)
-
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -20,30 +16,26 @@ func Frequency(s string) FreqMap {
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 
-// Compared the Benchmarks, but the Concurrent version has worse ns/op.
 // BenchmarkSequentialFrequency
-// BenchmarkSequentialFrequency-8              2437            472813 ns/op           17729 B/op         15 allocs/op
+// BenchmarkSequentialFrequency-8              2304            479404 ns/op           17728 B/op         15 allocs/op
 // BenchmarkConcurrentFrequency
-// BenchmarkConcurrentFrequency-8               902           1272479 ns/op            4552 B/op         24 allocs/op
+// BenchmarkConcurrentFrequency-8              5232            224873 ns/op           12609 B/op         66 allocs/op
 func ConcurrentFrequency(l []string) FreqMap {
 	freq := make(FreqMap, 0)
-	wg := sync.WaitGroup{}
-	mu := sync.Mutex{}
+	c := make(chan FreqMap)
 
 	for _, s := range l {
-		wg.Add(1)
-		go func(s string, freq FreqMap) {
-			defer wg.Done()
-
-			for _, r := range s {
-				mu.Lock()
-				freq[r]++
-				mu.Unlock()
-			}
-		}(s, freq)
+		go func(s string, c chan FreqMap) {
+			freq := Frequency(s)
+			c <- freq
+		}(s, c)
 	}
 
-	wg.Wait()
+	for range l {
+		for key, value := range <-c {
+			freq[key] += value
+		}
+	}
 
 	return freq
 }
