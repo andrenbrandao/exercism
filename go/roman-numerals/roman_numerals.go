804ef85ea98087b1c4cb1fa31e88b98e15c34dc1
André Brandão <andrenbrandao@gmail.com>
2022-08-07 11:33:25 -0300

    go/roman-numerals: add solution

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,115 @@
+package romannumerals
+
+import (
+	"errors"
+	"math"
+	"strings"
+)
+
+/*
+
+We want to convert from normal numbers to romans.
+
+1 -> I
+2 -> II
+3 -> III
+4 -> IV
+5 -> V
+6 -> VI
+...
+10 -> X
+50 -> L
+100 -> C
+500 -> D
+1000 -> M
+
+1990 -> MCMXC
+90 = XC
+900 = CM
+1000 = M
+
+Notice that we need to take the digits from right to left and form the roman
+numerals.
+
+1999 -> MCMXCIX
+
+Algorithm:
+- Take rightmost digit
+- If it is not zero, convert it to roman numeral
+- Iterate to the left digit and keep converting them
+- Reverse the string at the end
+
+To do this we will need a hashmap to convert the decimals to romans.
+
+If digit is [2,3] we repeat the roman numeral that represents 1,
+but if it is 4, we have to prefix the roman at position 5 with the roman at 1.
+
+The digits 6, 7 and 8 also add a suffix of digit 1 to a roman numeral 5.
+A digit 9 adds a prefix of digit 1 to a roman numeral of 10.
+
+*/
+
+var intToRoman = map[int]string{
+	1:    "I",
+	5:    "V",
+	10:   "X",
+	50:   "L",
+	100:  "C",
+	500:  "D",
+	1000: "M",
+}
+
+func ToRomanNumeral(input int) (string, error) {
+	digitPos := 0
+	sb := strings.Builder{}
+
+	if input <= 0 || input > 3000 {
+		return "", errors.New("can only convert integers from 1 to 3000")
+	}
+
+	for input != 0 {
+		lastDigit := input % 10
+
+		switch lastDigit {
+		case 1, 2, 3:
+			for i := 0; i < lastDigit; i++ {
+				sb.WriteString(digitToRoman(1, digitPos))
+			}
+		case 4:
+			sb.WriteString(digitToRoman(5, digitPos))
+			sb.WriteString(digitToRoman(1, digitPos))
+		case 5:
+			sb.WriteString(digitToRoman(5, digitPos))
+		case 6, 7, 8:
+			for i := 0; i < lastDigit-5; i++ {
+				sb.WriteString(digitToRoman(1, digitPos))
+			}
+			sb.WriteString(digitToRoman(5, digitPos))
+		case 9:
+			sb.WriteString(digitToRoman(10, digitPos))
+			sb.WriteString(digitToRoman(1, digitPos))
+		}
+
+		input /= 10
+		digitPos++
+	}
+
+	romanNumerals := reverseString(sb.String())
+	return romanNumerals, nil
+}
+
+func digitToRoman(digit, digitPos int) string {
+	return intToRoman[digit*int(math.Pow10(digitPos))]
+}
+
+func reverseString(s string) string {
+	sb := strings.Builder{}
+	n := len(s)
+	sb.Grow(n)
+
+	for i := n - 1; i >= 0; i-- {
+		sb.WriteByte(s[i])
+	}
+
+	return sb.String()
+}
