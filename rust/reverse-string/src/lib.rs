22797bd8e345ad468657039cdc9a4ecadccbe660
André Brandão <andrenbrandao@gmail.com>
2023-07-03 12:51:27 -0300

    rust/reverse-string: improve by using collect

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut res = String::with_capacity(input.len());
-
-    res.extend((*input).chars().rev());
-
-    res
+    (*input).chars().rev().collect::<String>()
 }
