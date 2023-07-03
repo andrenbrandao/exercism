94a63791045f04101b0dcdd38c2f082dab0ec744
André Brandão <andrenbrandao@gmail.com>
2023-07-03 12:55:18 -0300

    rust/reverse-string: solve it with grapheme clusters

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    (*input).chars().rev().collect::<String>()
+    (*input).graphemes(true).rev().collect::<String>()
 }
